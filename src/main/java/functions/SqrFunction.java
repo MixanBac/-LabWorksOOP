0f35208664f3f220ca8cd3218bcca1db0bd2eb61
1 <n>
2020-09-23 14:46:04 +0400

    Добавлен класс SqrFunction

---
+++ src/main/java/functions/SqrFunction.java
@@ -0,0 +1,13 @@
+package functions;
+
+public class SqrFunction implements MathFunction{
+
+    public SqrFunction(double x){
+    }
+
+    @Override
+    public MathFunction apply(double x) {
+        return new SqrFunction (Math.pow(x,2));
+    }
+
+}
