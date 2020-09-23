99053fb7a12f12b25769322529261d23a2b52e41
1 <n>
2020-09-23 14:16:38 +0400

    Добавлен класс HelloWorld

---
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/HelloWorld.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
