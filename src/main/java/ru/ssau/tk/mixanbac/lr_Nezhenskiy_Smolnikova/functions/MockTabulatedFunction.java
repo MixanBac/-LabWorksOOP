8f22d3e2bebdf982f40a69a974d213225bb82e24
1 <n>
2020-10-01 07:41:47 +0400

    ЛР 2.2 Тестирование методов interpolate() и apply().

---
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/functions/MockTabulatedFunction.java
@@ -0,0 +1,80 @@
+package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions;
+
+public class MockTabulatedFunction extends AbstractTabulatedFunction {
+
+    private static final double x0 = 1.0;
+    private static final double x1 = 2.0;
+    private static final double y0 = 3.0;
+    private static final double y1 = 4.0;
+
+    @Override
+    protected int floorIndexOfX(double x) {
+        return indexOfX(x1);
+    }
+
+    @Override
+    protected double extrapolateLeft(double x) {
+        return getY(indexOfX(x0));
+    }
+
+    @Override
+    protected double extrapolateRight(double x) {
+        return getY(indexOfX(x1));
+    }
+
+    @Override
+    protected double interpolate(double x, int floorIndex) {
+        return y0 + (y1 - y0) * (x - x0) / (x1 - x0);
+    }
+
+    @Override
+    public double getX(int index) {
+        if (index == 0) {
+            return x0;
+        } else {
+            return x1;
+        }
+    }
+
+    @Override
+    public double getY(int index) {
+        if (index == 0) {
+            return y0;
+        } else {
+            return y1;
+        }
+    }
+
+    @Override
+    public void setY(int index, double value) {
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        if (x == x0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        if (y == y0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    @Override
+    public double leftBound() {
+        return x0;
+    }
+
+    @Override
+    public double rightBound() {
+        return x1;
+    }
+}
+
---
+++ src/test/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/functions/AbstractTabulatedFunctionTest.java
@@ -0,0 +1,28 @@
+package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class AbstractTabulatedFunctionTest {
+    @Test
+    public void testInterpolate() {
+        AbstractTabulatedFunction mockedInterpolate = new MockTabulatedFunction();
+
+        final double delta = 0.0001;
+        assertEquals(mockedInterpolate.interpolate(1.4, 1.0, 2.0, 3.0, 4.0), 3.4, delta);
+        assertEquals(mockedInterpolate.interpolate(1.5, 1.0, 2.0, 3.0, 4.0), 3.5, delta);
+        assertEquals(mockedInterpolate.interpolate(1.6, 1.0, 2.0, 3.0, 4.0), 3.6, delta);
+        assertNotEquals(mockedInterpolate.interpolate(1.5, 1.0, 2.0, 3.0, 4.0), 8.1);
+    }
+
+    @Test
+    public void testApply() {
+        AbstractTabulatedFunction mockedApply = new MockTabulatedFunction();
+
+        final double delta = 0.0001;
+        assertEquals(mockedApply.apply(1.3), 4.0, delta);
+        assertEquals(mockedApply.apply(4.0), 4.0, delta);
+        assertNotEquals(mockedApply.apply(1.3), 5.2);
+    }
+}
