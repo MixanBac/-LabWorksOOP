09136125e379347981661328d6e21cf87410cacc
1 <n>
2020-10-28 16:37:47 +0400

    ЛР 3.2 Добавлено исключение InconsistentFunctionsException

---
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException{
+
+    public InconsistentFunctionsException() {
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+
+}
