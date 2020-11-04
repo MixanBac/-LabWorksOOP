6e8d2506548f369f194eb9e360a551d163aa0568
1 <n>
2020-11-04 19:07:52 +0400

    ЛР 3.3

--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
@@ -1,8 +1,13 @@
 package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.io;
 
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.Point;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.TabulatedFunction;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.factory.TabulatedFunctionFactory;
+
+import java.io.*;
+
 public class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
-
 }
