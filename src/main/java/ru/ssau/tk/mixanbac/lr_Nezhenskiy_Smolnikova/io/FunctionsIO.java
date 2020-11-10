929762446a04d32baa6ac57aa5ff703d0e60fcf8
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-11-10 21:28:58 +0400

    дабавила writeTabulatedFunction

--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
@@ -3,6 +3,7 @@ package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.io;
 import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.Point;
 import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.TabulatedFunction;
 import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.factory.TabulatedFunctionFactory;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.operations.TabulatedFunctionOperationService;
 
 import java.io.*;
 
@@ -19,5 +20,14 @@ public class FunctionsIO {
         }
         printWriter.flush();
 
+    }
+    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(function.getCount());
+        for (Point currentPoint : TabulatedFunctionOperationService.asPoints(function)) {
+            out.writeDouble(currentPoint.x);
+            out.writeDouble(currentPoint.y);
+        }
+        out.flush();
     }
 }
--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/TabulatedFunctionFileOutputStream.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/TabulatedFunctionFileOutputStream.java
@@ -2,13 +2,26 @@ package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.io;
 
 import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.TabulatedFunction;
 import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.factory.LinkedListTabulatedFunctionFactory;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class TabulatedFunctionFileOutputStream {
+    public static void main(String[] args) {
+        try (BufferedOutputStream listOut = new BufferedOutputStream(new FileOutputStream("output/linked list function.bin"));
+             BufferedOutputStream arrayOut = new BufferedOutputStream(new FileOutputStream("output/array function.bin"))) {
+            double[] xValues = new double[]{1., 2., 3., 4., 5.,6.,7.};
+            double[] yValues = new double[]{6., 7., 8., 9., 10., 11., 12.};
+            ArrayTabulatedFunctionFactory arrayFactory = new ArrayTabulatedFunctionFactory();
+            LinkedListTabulatedFunctionFactory linkedListFactory = new LinkedListTabulatedFunctionFactory();
+            TabulatedFunction arrayFunction = arrayFactory.create(xValues, yValues);
+            TabulatedFunction linkedListFunction = linkedListFactory.create(xValues, yValues);
+            FunctionsIO.writeTabulatedFunction(arrayOut, arrayFunction);
+            FunctionsIO.writeTabulatedFunction(listOut, linkedListFunction);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     }
 
