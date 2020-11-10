0d7fc734afd8b0fdacbaf4b94cc6712cfba4dad0
1 <n>
2020-11-10 21:47:43 +0400

    ЛР 3.3 Большие создан метод TabulatedFunctionFileReader

--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
@@ -6,6 +6,9 @@ import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.factory.TabulatedF
 import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.operations.TabulatedFunctionOperationService;
 
 import java.io.*;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
 
 public class FunctionsIO {
     private FunctionsIO() {
@@ -29,5 +32,23 @@ public class FunctionsIO {
             out.writeDouble(currentPoint.y);
         }
         out.flush();
+    }
+    public static TabulatedFunction readTabulatedFunction(BufferedReader reader, TabulatedFunctionFactory factory) throws IOException {
+        int count = Integer.parseInt(reader.readLine());
+
+        double[] xValues = new double[count];
+        double[] yValues = new double[count];
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.forLanguageTag("ru"));
+        for (int i = 0; i < count; i++) {
+            String line = reader.readLine();
+            String[] splitLine = line.split(" ");
+            try {
+                xValues[i] = numberFormat.parse(splitLine[0]).doubleValue();
+                yValues[i] = numberFormat.parse(splitLine[1]).doubleValue();
+            } catch (ParseException eParse) {
+                throw new IOException(eParse);
+            }
+        }
+        return factory.create(xValues, yValues);
     }
 }
