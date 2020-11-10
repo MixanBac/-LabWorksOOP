91598332eff4e914972171966386c0fa62fbb478
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-11-10 21:03:29 +0400

    готов класс  TabulatedFunctionFileWriter

--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
@@ -10,15 +10,14 @@ public class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
     }
-    public static void writeTabulatedFunction(BufferedOutputStream outputStream, TabulatedFunction function) throws IOException {
-        DataOutputStream out = new DataOutputStream(outputStream);
-        out.writeInt(function.getCount());
-        for (Point point : function) {
-            out.writeDouble(point.x);
-            out.writeDouble(point.y);
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) {
+        PrintWriter printWriter = new PrintWriter(writer);
+        printWriter.println(function.getCount());
+        int i = 0;
+        for (Point a : function) {
+            printWriter.printf("%f %f\n", a.x, a.y);
         }
-        out.flush();
+        printWriter.flush();
+
     }
-
-
 }
---
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/TabulatedFunctionFileWriter.java
@@ -0,0 +1,34 @@
+package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.io;
+
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.ArrayTabulatedFunction;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TabulatedFunctionFileWriter { public static void main(String[] args) {
+    File fileArray = new File("output/array function.txt");
+    File fileList = new File("output/linked list function.txt");
+
+    double[] xValue = new double[]{1, 2, 3, 4, 5};
+    double[] yValue = new double[]{6, 7, 8, 9, 10};
+
+    TabulatedFunction functionList = new LinkedListTabulatedFunction(xValue, yValue);
+    TabulatedFunction functionArray = new ArrayTabulatedFunction(xValue, yValue);
+
+    try (BufferedWriter outArray = new BufferedWriter(
+            new FileWriter(fileArray));
+         BufferedWriter outList = new BufferedWriter(
+                 new FileWriter(fileList))) {
+
+        FunctionsIO.writeTabulatedFunction(outArray, functionArray);
+        FunctionsIO.writeTabulatedFunction(outList, functionList);
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+}
