81d3047256e4e06c2ef745c8339be655727711a5
1 <n>
2020-11-11 15:54:16 +0400

    ЛР 3.3 Добавлен метод serialize

--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/io/FunctionsIO.java
@@ -63,4 +63,9 @@ public final class FunctionsIO {
 
         return factory.create(xValues, yValues);
     }
+    public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException {
+        ObjectOutputStream out = new ObjectOutputStream(stream);
+        out.writeObject(function);
+        out.flush();
+    }
 }
