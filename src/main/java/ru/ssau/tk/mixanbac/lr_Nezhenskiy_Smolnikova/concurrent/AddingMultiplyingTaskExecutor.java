0c57dd64684585bf99a150d86d5b34e5f59fe9d1
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-11-30 14:04:38 +0400

    4.1

---
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/concurrent/AddingMultiplyingTaskExecutor.java
@@ -0,0 +1,26 @@
+package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.concurrent;
+
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.ConstantFunction;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.TabulatedFunction;
+
+public class AddingMultiplyingTaskExecutor {
+    public static void main(String[] args) throws InterruptedException {
+        TabulatedFunction function = new LinkedListTabulatedFunction(new ConstantFunction(2), 1, 100, 100);
+
+        AddingTask addingTask = new AddingTask(function);
+        MultiplyingTask multiplyingTask = new MultiplyingTask(function);
+
+        Thread thread1 = new Thread(multiplyingTask);
+        thread1.start();
+        Thread thread2 = new Thread(multiplyingTask);
+        thread2.start();
+        Thread thread3 = new Thread(addingTask);
+        thread3.start();
+
+        Thread.sleep(2000);
+
+        System.out.println(function.toString());
+    }
+}
+
