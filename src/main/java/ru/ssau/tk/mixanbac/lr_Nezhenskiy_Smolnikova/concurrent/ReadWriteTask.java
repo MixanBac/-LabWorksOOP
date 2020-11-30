cb3cd1fc3595dbfae6e111d2b03cb16f77b2013b
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-11-30 13:56:31 +0400

    начала делать

---
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/concurrent/ReadWriteTask.java
@@ -0,0 +1,29 @@
+package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.concurrent;
+
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.TabulatedFunction;
+
+public class ReadWriteTask implements Runnable {
+
+    private final TabulatedFunction function;
+
+    public ReadWriteTask(TabulatedFunction function) {
+        this.function = function;
+    }
+
+    @Override
+    public void run() {
+        double x;
+        double y;
+        for (int i = 0; i < function.getCount(); i++) {
+            x = function.getX(i);
+            synchronized (function) {
+                y = function.getY(i);
+                System.out.printf("%s, before write: i = %d, x = %f, y = %f \n", Thread.currentThread().getName(), i, x, y);
+                function.setY(i, y + 1);
+                y = function.getY(i);
+            }
+            System.out.printf("%s, after write: i = %d, x = %f, y = %f \n", Thread.currentThread().getName(), i, x, y);
+        }
+    }
+}
+
