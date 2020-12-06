c4c56d332ae99957134a345c943bd3377281efef
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-12-06 23:34:51 +0400

    что то начала

--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/ui/CreatingFunction.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/ui/CreatingFunction.java
@@ -1,14 +1,33 @@
 package ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.ui;
 
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.exceptions.ArrayIsNotSortedException;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.TabulatedFunction;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.mixanbac.lr_Nezhenskiy_Smolnikova.functions.factory.TabulatedFunctionFactory;
+
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import static java.lang.module.ModuleFinder.compose;
 
 public class CreatingFunction extends JFrame {
-    public static void main(String[] args) {
-
-        JFrame startFrame = new JFrame("Наш интерфейс");
-        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Установим константы по определению при нажатии крестика, для закрытия приложения
-        startFrame.pack();//Фрейм по размерам будет занимать столько, сколько и его компоненты
-        startFrame.setLocationRelativeTo(null);//Позиция по центру
-        startFrame.setVisible(true);//Окно видимое
+    List<Double> xValues = new ArrayList<>();
+    List<Double> yValues = new ArrayList<>();
+    private JLabel label = new JLabel("Введите колличество точек:");
+    private JTextField countField = new JTextField();
+    private JButton inputButton = new JButton("Ввод");
+    private JButton createButton = new JButton("Создать");
+    TabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+    TabulatedFunction function;
+    public void TabulatedFunctionWindow() {
+        this.setBounds(500, 500, 500, 500);
     }
+
 }
