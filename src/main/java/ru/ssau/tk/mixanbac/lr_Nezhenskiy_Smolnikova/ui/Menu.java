ec598e644dcd2d6117493d04f4cb0aaf9cefd2d3
1 <n>
2020-12-18 17:46:13 +0400

    Все пошло не по плану

--- src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/ui/Menu.java
+++ src/main/java/ru/ssau/tk/mixanbac/lr_Nezhenskiy_Smolnikova/ui/Menu.java
@@ -13,7 +13,65 @@ import java.util.List;
 import static java.lang.module.ModuleFinder.compose;
 
 public class Menu extends JFrame {
+    private JFrame frame;
+    private JButton inputButton = new JButton("Create the table");
+    private JButton inputButtonFactory = new JButton("Choose factory");
+    private JButton inputButtonMath = new JButton("Choose Math function");
+    private TabulatedFunctionFactory factory;
 
+    public Menu() {
+        setTitle("Menu");
+        setBounds(500, 500, 500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        actionPerformed();
+        compose();
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public static void main(String[] args) {
+        Menu window = new Menu();
+        window.setVisible(true);
+
+    }
+
+
+    public void actionPerformed() {
+        inputButton.addActionListener(event -> {
+                    try {
+                        MyFrame.main(factory);
+                        frame.setVisible(false);
+                    } catch (Exception e) {
+                        new Error(this, e);
+                    }
+                }
+        );
+        inputButtonMath.addActionListener(event -> {
+            try {
+                MathFunctionWindow.main(factory);
+            } catch (Exception e) {
+                new Error(this, e);
+            }
+        });
+    }
+
+    void compose() {
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+        layout.setHorizontalGroup(layout.createParallelGroup(GroupLayout.Alignment.CENTER)
+                .addGroup(layout.createSequentialGroup()
+                        .addComponent(inputButton))
+                .addComponent(inputButtonFactory)
+                .addComponent(inputButtonMath)
+        );
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(inputButton))
+                .addComponent(inputButtonFactory)
+                .addComponent(inputButtonMath)
+        );
+    }
 }
 
 
